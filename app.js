6db9675e7e8c89746b0b5aa8c4c2d4a8d9f9e9b8
ehdguss <by606700@naver.com>
2020-03-16 23:21:53 +0900

    change color

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //canvas rendering context 2d
-
 const canvas = document.getElementById("js-canvas");
 const ctx = canvas.getContext("2d");
+
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -32,8 +33,9 @@ function onMouseMove(event) {
     }
 }
 
-function onMouseDown(event) {
-    painting = true;
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
 }
 
 if(canvas) {
@@ -42,3 +44,5 @@ if(canvas) {
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick)); 
